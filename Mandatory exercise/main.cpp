240dccc43fb54321e7eaa319e263398d0a74969d
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-03-01 14:15:37 +0100

    Opret projekt.

---
+++ Mandatory exercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mandatory exercise
+//
+//  Created by Anders Launer Bæk on 01/03/15.
+//  Copyright (c) 2015 Anders Launer Bæk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
