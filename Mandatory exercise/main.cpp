bb82384291b852081cdc695a2c929f46d5878961
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-03-02 13:10:46 +0100

    problem text

--- Mandatory exercise/main.cpp
+++ Mandatory exercise/main.cpp
@@ -31,10 +31,30 @@ int main() {
     }
     
     // SDV of problem
-
+    /* Find expressions for the elements in the matrix A and the right-hand side, say z,
+       for the associated systems of linear equations for the pa- rameters q = (x0, y0, a, b).
+    */
     
     
     
+    /* Read the files with values, insert them in A and compute U, W og V using the method from NR. 
+       State (with arguments based on the SVD matrices) if there are linear dependencies between 
+       the parameters x0, y0, a, b.
+    */
+    
+    
+    
+    /* Estimate the parameters q = (x0, y0, a, b) and state your results. 
+       State also the residual error ∥Aq − z∥.
+    */
+    
+    
+    
+    
+    /* The values of the x(i), y(i)’s are measured with a camera, and there is a measurement uncertainty 
+       that is estimated to 1mm on each coordinate. Estimate the resulting errors on the found parameters 
+       using NR, Eq. (15.4.19).
+    */
     
     
     std::cout <<"Hello world..." << std::endl;
