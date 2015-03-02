a2fceafd369ca28bc4ad623f94b6a3547d236385
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-03-02 13:01:02 +0100

    Reading the file...

--- Mandatory exercise/main.cpp
+++ Mandatory exercise/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include "nr3.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Reading the data file
+    const int dataRows = 500;
+    const int dataColumn = 4;
+    MatDoub A(dataRows,dataColumn);
+    
+    std::ifstream dataPoints("/Users/Anders/Dropbox/6.\ semester/NUM6/Mandatory\ exercise/d1.txt"); // Anders
+//    std::ifstream dataPoints("/Users//d1.txt"); // Mikkel
+    if ( ! dataPoints.is_open() ) {
+        cout <<" Failed to open data file..." << endl;
+        exit(0);
+    }
+    
+    for (int i=0; i<dataRows; i++) {
+        for(int j = 0; j < dataColumn; j++) {
+            dataPoints >> A[i][j];
+        }
+    }
+    
+    // SDV of problem
+
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
