5a4c1b7679983e93ad0fcb9064f56bf5c12c9c09
Anders Launer Bæk <anbae12@student.sdu.dk>
2015-03-04 12:30:47 +0100

    final

--- Mandatory exercise/main.cpp
+++ Mandatory exercise/main.cpp
@@ -22,8 +22,7 @@ int main() {
     VecDoub y(dataRows);
     
     
-    std::ifstream dataPoints("/Users/Anders/Dropbox/6.\ semester/NUM6/Mandatory\ exercise/d2.txt"); // Anders
-//    std::ifstream dataPoints("/Users//d1.txt"); // Mikkel
+    std::ifstream dataPoints("/Users/Anders/Dropbox/6.\ semester/NUM6/Mandatory\ exercise/d1.txt"); // Anders
     if ( ! dataPoints.is_open() ) {
         std::cout <<"Failed to open data file..." << std::endl;
         exit(0);
@@ -36,14 +35,14 @@ int main() {
     }
     // SDV of problem
     /* Find expressions for the elements in the matrix A and the right-hand side, say z,
-       for the associated systems of linear equations for the pa- rameters q = (x0, y0, a, b).
-    */
+     for the associated systems of linear equations for the pa- rameters q = (x0, y0, a, b).
+     */
     // Latex
     
-    /* Read the files with values, insert them in A and compute U, W og V using the method from NR. 
-       State (with arguments based on the SVD matrices) if there are linear dependencies between 
-       the parameters x0, y0, a, b.
-    */
+    /* Read the files with values, insert them in A and compute U, W og V using the method from NR.
+     State (with arguments based on the SVD matrices) if there are linear dependencies between
+     the parameters x0, y0, a, b.
+     */
     MatDoub A(dataRows*2,dataColumn);
     int tempIt=0;
     for (int j=0; j<dataRows*2; j++) {
@@ -61,13 +60,13 @@ int main() {
         }
     }
     
-    VecDoub b(dataRows*2);
+    VecDoub z(dataRows*2);
     int tempIt2=0;
     for (int j=0; j<dataRows*2; j++) {
         if (j%2==0) {
-            b[j]=x[tempIt2];
+            z[j]=x[tempIt2];
         } else{
-            b[j]=y[tempIt2];
+            z[j]=y[tempIt2];
             tempIt2++;
         }
     }
@@ -78,16 +77,16 @@ int main() {
     
     std::cout<<"W\n"<<W<<std::endl;
     std::cout<<"V\n"<<V<<std::endl;
-//    std::cout<<"U\n"<<U<<std::endl;
+    std::cout<<"U\n"<<U<<std::endl;
     
     
-    /* Estimate the parameters q = (x0, y0, a, b) and state your results. 
-       State also the residual error ∥Aq − z∥.
-    */
+    /* Estimate the parameters q = (x0, y0, a, b) and state your results.
+     State also the residual error ∥Aq − z∥.
+     */
     VecDoub q(dataColumn);
-    obj.solve(b, q);
+    obj.solve(z, q);
     std::cout<<"q\n"<<q<<std::endl;
-
+    
     VecDoub residual(dataRows*2);
     tempIt=0;
     Doub x0val=q[0];
@@ -114,10 +113,10 @@ int main() {
     }
     std::cout<<"Residual error\n"<<Sum/(dataRows*2)<<"\n"<<std::endl;
     
-    /* The values of the x(i), y(i)’s are measured with a camera, and there is a measurement uncertainty 
-       that is estimated to 1mm on each coordinate. Estimate the resulting errors on the found parameters 
-       using NR, Eq. (15.4.19).
-    */
+    /* The values of the x(i), y(i)’s are measured with a camera, and there is a measurement uncertainty
+     that is estimated to 1mm on each coordinate. Estimate the resulting errors on the found parameters
+     using NR, Eq. (15.4.19).
+     */
     VecDoub stddev(dataColumn);
     for (int i=0; i<dataColumn; i++) {
         Doub varians = 0;
@@ -126,9 +125,6 @@ int main() {
         }
         stddev[i]=sqrt(abs(varians));
     }
-    
     std::cout<<"STD\n"<<stddev<<std::endl;
-    
-    
     return 0;
 }
